be93621589ea62b9f2090cdedd98ee484e2d9e6d
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-05-13 03:56:45 +0000

    Update ec2-termination-protection-enable-disable.sh

--- ec2-termination-protection-enable-disable.sh
+++ ec2-termination-protection-enable-disable.sh
@@ -1,4 +1,9 @@
+#########
+# Enable
+#########
 #!/bin/bash
+# Enable termination protection
+
 set -x
 # Purpose: To protect all my existing resources from accidental termination
 
@@ -10,6 +15,34 @@ do
   echo "protecting ec2 in region $REGION"
   aws ec2 modify-instance-attribute --disable-api-termination --instance-id $ec2
 done
+
+#########
+# Disable
+#########
+------------------------------------------------------------------------------------------------------------------------------------------
+
+#!/bin/bash
+# Disable termination protection
+
+set -x
+# Purpose: To protect all my existing resources from accidental termination
+
+REGION="us-east-1"
+INSTANCE_ID=$(aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].InstanceId' --region $REGION)
+
+for ec2 in $INSTANCE_ID
+do
+  echo "protecting ec2 in region $REGION"
+  aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $ec2
+done
+
+
+
+
+
+
+
+
 
 
 # Useful links
